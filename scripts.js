0718d282f0dab099ada5f20a8f9f563247ad9ded
Khaleel Smith <145707575+ksmithereens@users.noreply.github.com>
2023-11-13 19:43:03 +0000

    JS added

---
+++ scripts.js
@@ -0,0 +1,9 @@
+console.log("👍 JS Connected")
+
+document.querySelectorAll(".card").forEach((item) => {
+
+    item.addEventListener("click", (event) => {
+
+        item.classList.toggle("flipped");
+    });
+});
